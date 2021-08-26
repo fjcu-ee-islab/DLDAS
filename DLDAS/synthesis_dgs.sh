35ae13eb384e93eee893ca1905e653274d5d5a87
aven1995 <aven@islab.tw>
2021-08-26 22:39:07 +0800

    fourth commit

--- DLDAS/synthesis_dgs.sh
+++ DLDAS/synthesis_dgs.sh
@@ -8,5 +8,5 @@ mkdir dgs_paper_synthesis/$trainum
 python test_batch.py --config configs/dgs_paper.yaml --input_folder datasets/dgs/testA --output_folder dgs_paper_synthesis/$trainum --num_style 9 --checkpoint outputs/dgs_paper/checkpoints/gen_000$trainum.pt --a2b 1
 
 python transfer_data.py $trainum dgs 9
-python elasctic_trans.py rim
+python elasctic_trans.py dgs
 
--- DLDAS/train_dgs.sh
+++ DLDAS/train_dgs.sh
@@ -1,6 +1,2 @@
-#CUDA_VISIBLE_DEVICES=1 python train.py --config configs/rim1.yaml
-#CUDA_VISIBLE_DEVICES=1 python train.py --config configs/rim2.yaml
-#CUDA_VISIBLE_DEVICES=1 python train.py --config configs/rim3.yaml
-#CUDA_VISIBLE_DEVICES=1 python train.py --config configs/rim4.yaml
 python train.py --config configs/dgs_paper.yaml
 
